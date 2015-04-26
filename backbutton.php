0fa061cb13474f05e73a52506a072ac803d0d9df
wojsmol <wojsmol@wp.pl>
2015-04-26 21:52:30 +0200

    Add version 1.1 for Joomla! 1.7+

--- backbutton.php
+++ backbutton.php
@@ -14,7 +14,7 @@
  */
  
  // no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
+defined( '_JEXEC' ) or die;
 jimport( 'joomla.plugin.plugin' );
 class plgButtonBackbutton extends JPlugin {
     /**
