2d13745fd4ef3b9513efbe84df0514ffd7fa303b
Wojciech Smoliński <wojsmol@wp.pl>
2017-05-14 19:10:44 +0200

    aplly Joomla CS and small refactoring

--- backbutton.php
+++ backbutton.php
@@ -3,54 +3,65 @@
  * @version             $Id: backbutton.php wojsmol 2011-06-20 13:31:00 wojsmol$
  * Based on http://tushev.org/articles/programming/18-how-to-create-an-editor-button-editors-xtd-plugin-for-joomla
  * @package             Joomla
- * @subpackage  System
- * @copyright   Copyright (C) Wojciech Smoliñski, 2011. All rights reserved.
- * @license     GNU GPL v2.0
+ * @subpackage          System
+ * @copyright           Copyright (C) Wojciech SmoliÅ„ski, 2011. All rights reserved.
+ * @license             GNU GPL v2.0
  * Joomla! is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  * See COPYRIGHT.php for copyright notices and details.
  */
- 
- // no direct access
-defined( '_JEXEC' ) or die;
-jimport( 'joomla.plugin.plugin' );
-class plgButtonBackbutton extends JPlugin {
-    /**
-     * Constructor
-     *
-     * For php4 compatability we must not use the __constructor as a constructor for plugins
-     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
-     * This causes problems with cross-referencing necessary for the observer design pattern.
-     *
-     * @param     object $subject The object to observe
-     * @param     array  $config  An array that holds the plugin configuration
-     * @since 1.5
-     */
-    function plgButtonBackbutton(& $subject, $config)
-    {
-        parent::__construct($subject, $config);
+
+// no direct access
+defined('_JEXEC') or die;
+jimport('joomla.plugin.plugin');
+
+class plgButtonBackbutton extends JPlugin
+{
+	/**
+	 * Constructor
+	 *
+	 * For php4 compatability we must not use the __constructor as a constructor for plugins
+	 * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
+	 * This causes problems with cross-referencing necessary for the observer design pattern.
+	 *
+	 * @param     object $subject The object to observe
+	 * @param     array  $config  An array that holds the plugin configuration
+	 *
+	 * @since 1.5
+	 */
+	function plgButtonBackbutton(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
 		$this->loadLanguage();
-    }
-    function onDisplay($name)
-    {
-        $js =  "                      
+	}
+
+	/**
+	 * @param $name
+	 *
+	 * @return JObject
+	 */
+	function onDisplay($name)
+	{
+		$js  = <<<JS
+                      
  function backbuttonXTDButtonClick(editor) {         
                               jInsertEditorText('{backbutton}', editor);
-		}";
-        $doc = JFactory::getDocument();
-        $doc->addScriptDeclaration($js);
-		
-        $button = new JObject();
-        $button->set('modal', false);
-		$button->set('class', 'btn');
-		$button->set('link', '#');
-        $button->set('text', JText::_('EBBBNAME'));
-		$button->set('name', 'backbuttonButton');
-		$button->set('onclick', 'backbuttonXTDButtonClick(\''.$name.'\');return false;');
-		
-        return $button;
-    }
+		}
+JS;
+		$doc = JFactory::getDocument();
+		$doc->addScriptDeclaration($js);
+
+		$button = new JObject();
+		$button->modal = false;
+		$button->class = 'btn';
+		$button->link = '#';
+		$button->text = JText::_('EBBBNAME');
+		$button->name = 'backbuttonButton';
+		$button->onclick = 'backbuttonXTDButtonClick(\'' . $name . '\');return false;';
+
+		return $button;
+	}
 }
-?>
+
