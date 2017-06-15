33fd4df8692694bf68a6db40c022e26a19be870f
Wojciech Smoliński <wojsmol@wp.pl>
2017-06-15 17:40:25 +0200

    delete comment

--- backbutton.php
+++ backbutton.php
@@ -20,9 +20,6 @@ class plgButtonBackbutton extends JPlugin {
     /**
      * Constructor
      *
-     * For php4 compatability we must not use the __constructor as a constructor for plugins
-     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
-     * This causes problems with cross-referencing necessary for the observer design pattern.
      *
      * @param     object $subject The object to observe
      * @param     array  $config  An array that holds the plugin configuration
